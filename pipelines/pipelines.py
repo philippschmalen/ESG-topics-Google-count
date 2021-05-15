9f8b3c1f7f43be7febe0082701e1ad189f7cb7d6
philippschmalen <philippschmalen@gmail.com>
2021-05-15 15:46:26 +0200

    syntax

--- pipelines/pipelines.py
+++ pipelines/pipelines.py
@@ -105,7 +105,7 @@ def main():
         GOOGLE_URL = settings['query']['google_url']
 
     # ~----------------- FLOW -----------------~
-    ~-- daily schedule
+    # ~-- daily schedule
     schedule = IntervalSchedule(
         start_date=datetime.strptime("20210424-041000UTC", "%Y%m%d-%H%M%S%Z"),
         interval=timedelta(days=1),
